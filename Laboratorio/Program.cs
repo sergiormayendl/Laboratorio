d0006b22e3511be4985f647bedefc6de989b4711
sergi <sergi@DESKTOP-E1MUF4I>
2023-02-18 11:21:54 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo soy Sergio");
+
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("soy Sergio");
+
+        /* COMENTARIOS */
+
+        Console.Write("Hola Mundo ");
+        Console.Write("soy Sergio\n");
+
+        Console.WriteLine("\nIngrese su nombre: ");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("soy " + Nombre);
+        Console.ReadKey();
+
+    }
+}
